a1bfec7f170f7875d6dd866a116d21d5d2d550a5
Shannon Mulloy <smulloy21@gmail.com>
2026-07-26 19:57:05 -0400

    add route for deleting blogs

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -91,6 +91,20 @@ test('blog post request fails if title or url is missing', async () => {
     .expect(400)
 })
 
+test('a valid blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const ids = blogsAtEnd.map(n => n.id)
+  assert(!ids.includes(blogToDelete.id))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
