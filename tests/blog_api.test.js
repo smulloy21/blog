646157c6ed4be1a067b71a2086edf9d064cf050d
Shannon Mulloy <smulloy21@gmail.com>
2026-07-26 18:10:16 -0400

    add post route integration test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -26,6 +26,34 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('blogs use id not _id as a unique identifier', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.notEqual(response.body[0].id, response.body[1].id)
+  assert.strictEqual(response.body[0]._id, undefined)
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'A new blog post',
+    author: 'Summer',
+    url: 'summers-blog.com',
+    likes: 0,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map(n => n.title)
+  assert(contents.includes('A new blog post'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
